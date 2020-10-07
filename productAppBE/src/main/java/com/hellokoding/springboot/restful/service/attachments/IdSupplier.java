b5be04b24b7deb80ac690b02a20feeb8df74ff60
Igor Lisakov <igor.lisakov@sigma.software>
2020-10-07 11:07:57 +0300

    Minor comment.

--- productAppBE/src/main/java/com/hellokoding/springboot/restful/service/attachments/IdSupplier.java
+++ productAppBE/src/main/java/com/hellokoding/springboot/restful/service/attachments/IdSupplier.java
@@ -13,6 +13,7 @@ public class IdSupplier implements Supplier<String> {
     @Override
     public String get() {
         // https://stackoverflow.com/questions/4267475/generating-8-character-only-uuids
+        // note that such id is not guaranteed to be unique!
         return RandomStringUtils.randomAlphanumeric(8).toLowerCase();
     }
 
